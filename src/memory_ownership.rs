771bd5aeba1780dab9b0549ea7829e328d1aaef2
Ain Tohvri <ain@flashbit.net>
2021-01-02 21:00:40 +0100

    Add borrowing example to memory ownership

--- src/memory_ownership.rs
+++ src/memory_ownership.rs
@@ -6,4 +6,10 @@ pub fn primitive_ownership() {
     let ione = 1;
     let itwo = ione;
     println!("Primitive reference to 'ione' variable succeeds for following debug print: {:?}", ione);
+
+    let mut vec2 = vec!["this", "can", "be", "iterated", "but", "not", "pushed"];
+    for i in vec2 {
+        println!("i in vec2 is {}", i);
+        //&vec2.push("sure?"); // fails compiler
+    }
 }
