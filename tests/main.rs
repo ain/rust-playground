5e0d3e696e0cd7ffa03ad8b5588b62f2027b80e5
Ain Tohvri <ain@flashbit.net>
2021-01-13 23:38:23 +0100

    Remove extern crate instruction
    Not required with workspaces

--- tests/main.rs
+++ tests/main.rs
@@ -1,8 +1,6 @@
 #[cfg(test)]
 mod tests {
 
-    extern crate functions;
-
     #[test]
     fn ascii_alphabet() {
         let expectation = vec!['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
